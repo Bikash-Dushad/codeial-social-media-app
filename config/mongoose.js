a244bef4a3d45324873a24034f78b0a81cdc98c4
Bikash-Dushad <bikashghll@gmail.com>
2024-05-13 16:56:48 +0530

    starting deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://127.0.0.1:27017/codeial_practice'; // Replace with your MongoDB connection URI
+const dbURI = process.env.MONGO_URL; // Replace with your MongoDB connection URI
 
 mongoose.connect(dbURI)
     .then(() => {
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express');
+const env = require('./config/environment');
 const app = express();
 const cookieparser = require('cookie-parser')
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 require('./config/mongoose');
 const expresssession = require('express-session')
 const passport = require('passport');
@@ -13,10 +15,10 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieparser())
 app.set('view engine', 'ejs');
 app.set('views', './views'); 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + env.asset_path));
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use(expresssession({
-    secret: 'xyz', // Replace with a random string for session encryption
+    secret: env.session_cookie_key, // Replace with a random string for session encryption
     resave: false,
     saveUninitialized: false
   }));
@@ -30,6 +32,6 @@ app.use(custumMiddleware.setFlash);
 
 app.use('/', require('./routes'))
 
-app.listen(port, ()=>{
-    console.log(`Server is running on http://localhost:${port}`)
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
